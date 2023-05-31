356273dae312017f782e2625cb743f2a9ead9518
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-05-31 11:45:06 +0300

    Adding paragraphs and styling with Css3. Close #3

--- template.js
+++ template.js
@@ -1,1 +1,7 @@
-alert("Hello\nI am an alert box!")
+/*
+Project Name: Full Stack Developer Blog Page
+-Name: Blog Page
+-Author: Hilmi Başkoparan
+-Version: v.1
+-Description: This project is a blog page about full stack developer.
+*/
